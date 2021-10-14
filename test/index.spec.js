4901e24079587494aa6dc06ab7be0c5f513fa97f
Amnat Wongkhamkaeo <61160025@go.buu.ac.th>
2021-10-14 18:39:23 +0700

    new ver homework

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const app = require('../index')
+
+describe('GET /', () => {
+    it('responds with "Hello world!!!"', (done) => {
+        request(app).get('/').expect('Hello world!!!', done)
+    })
+})
+
+
+describe('GET /users', () => {
+    it('responds with "No user in the system"', (done) => {
+        request(app).get('/users').expect('No user in the system', done)
+    })
+}) 
